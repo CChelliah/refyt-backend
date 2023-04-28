9651383bb6cadd205c0100157b0c3cd2e873f6f5
Cavinash Chelliah <cavinashchelliah1994@gmail.com>
2023-04-29 00:43:20 +1000

    Update

--- products/routes/getById.go
+++ products/routes/getById.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"refyt-backend/products/repo"
@@ -23,8 +22,6 @@ func Get(productRepo repo.ProductRepository) gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, "internal server error")
 			return
 		}
-
-		fmt.Println(fmt.Sprintf("Product %s", product.ImageUrls[0]))
 
 		c.JSON(200, product)
 	}
