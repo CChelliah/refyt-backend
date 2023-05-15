c2348cd4ec93aad020dbd171b24e78b65fd9c2b3
Cavinash Chelliah <cavinashchelliah1994@gmail.com>
2023-05-15 23:49:51 +1000

    Refactoring, add logger, add EDA

--- bookings/repo/sql.go
+++ bookings/repo/sql.go
@@ -4,6 +4,7 @@ const (
 	findBookingsBySellerID = `SELECT booking_id, b.product_id, customer_id, start_date, end_date, status, created_at, updated_at, sp.product_name, shipping_method, images FROM
 								(SELECT product_id, product_name, images FROM product_service.products p WHERE customer_id = $1) as sp
 								INNER JOIN booking_service.bookings b ON sp.product_id = b.product_id
+                                WHERE b.status != 'Created'
 								ORDER BY b.start_date DESC;`
 
 	findBookingsByProductID = `SELECT booking_id, product_id, customer_id, start_date, end_date, status, created_at, updated_at FROM
